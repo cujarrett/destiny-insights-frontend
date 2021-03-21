9f667def228e80ba2291891d603a945c9b9cdbf3
Matt Jarrett <cujarrett@gmail.com>
2021-03-20 22:17:01 -0500

    feat: adds terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "site_name" {
+  description = "Site name"
+  type = string
+  default = "banshee44mods.com"
+}
+
+variable "aws_region" {
+  description = "AWS region for the infrastructure"
+  type = string
+  default = "us-east-1"
+}
