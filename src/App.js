f1f4bb298d1338980992ba9133a0d6da3b41a01e
Matt Jarrett <16245634+cujarrett@users.noreply.github.com>
2021-07-11 02:36:00 -0500

    feat: updates backend (#7)
    * feat: updates backend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React, { useState, useEffect } from "react"
-import { About } from "./components/about/index.js"
 import { Cards } from "./components/cards/index.js"
 import { Footer } from "./components/footer/index.js"
 import { Header } from "./components/header/index.js"
 import { Info } from "./components/info/index.js"
-import { Menu } from "./components/menu/index.js"
 import { Search } from "./components/search/index.js"
-import { Support } from "./components/support/index.js"
 import { Timeline } from "./components/timeline/index.js"
-import { TwitterBot } from "./components/twitter-bot/index.js"
 import "./App.css"
 
-import { getCardData, getModData, getTimelineData, getTypeData } from "./integrations/banshee-44-mods.js"
+// eslint-disable-next-line max-len
+import { getCardData, getModData, getChargedWithLightModTimelineData, getWarmindCellModTimelineData } from "./integrations/destiny-insights.js"
 
 const App = () => {
   const [width, setWidth] = useState(0)
   const [height, setHeight] = useState(0)
-  const [timelineData, setTimelineData] = useState([])
+  const [chargedWithLightModTimelineData, setChargedWithLightModTimelineData] = useState([])
+  const [warmindCellModTimelineData, setWarmindCellModTimelineData] = useState([])
   const [cardData, setCardData] = useState([])
-  const [modTypeData, setModTypeData] = useState([])
   const [searchTerms, setSearchTerms] = useState("")
 
   const handleWidthResize = () => {
@@ -36,25 +33,25 @@ const App = () => {
 
   useEffect(async () => {
     const modData = await getModData()
-    const timelineChartData = getTimelineData(modData)
-    setTimelineData(timelineChartData)
+    const chargedWithLightChartData = getChargedWithLightModTimelineData(modData)
+    setChargedWithLightModTimelineData(chargedWithLightChartData)
+    const warmindCellChartData = getWarmindCellModTimelineData(modData)
+    setWarmindCellModTimelineData(warmindCellChartData)
     const modCardData = getCardData(modData)
     setCardData(modCardData)
-    const typeData = getTypeData(modData)
-    setModTypeData(typeData)
   }, [])
 
   return (
     <div className="App">
       <Header />
-      <Menu />
-      <Info armorMods={modTypeData.armorMods} weaponMods={modTypeData.weaponMods}/>
-      <Timeline width={width} height={height} timelineData={timelineData}/>
-      <Search setSearchTerms={setSearchTerms}/>
-      <Cards cardData={cardData} searchTerms={searchTerms}/>
-      <About />
-      <TwitterBot />
-      <Support />
+      <Timeline
+        width={width}
+        height={height}
+        chargedWithLightModTimelineData={chargedWithLightModTimelineData}
+        warmindCellModTimelineData={warmindCellModTimelineData} />
+      <Search setSearchTerms={setSearchTerms} />
+      <Cards cardData={cardData} searchTerms={searchTerms} />
+      <Info />
       <Footer />
     </div>
   )
---
+++ src/integrations/destiny-insights.js
@@ -0,0 +1,116 @@
+const fetch = require("node-fetch")
+const cachedData = require("../data/cached-mods.json")
+
+const getModData = async () => {
+  const endpoint = "https://api.destinyinsights.com/mod-data-for-last-year"
+  const options = {
+    method: "GET",
+    json: true
+  }
+
+  try {
+    const rawResponse = await fetch(endpoint, options)
+    const { data } = await rawResponse.json()
+
+    const cachedMods = {}
+    for (const value of Object.values(cachedData)) {
+      cachedMods[value.name] = {
+        type: value.type,
+        description: value.description,
+        icon: value.icon
+      }
+    }
+
+    for (const mod of data) {
+      mod.type = cachedMods[mod.name].type
+      mod.description = cachedMods[mod.name].description
+      mod.icon = cachedMods[mod.name].icon
+    }
+
+    return data
+  } catch (error) {
+    throw new Error("api.destinyinsights.com/mod-data-for-last-year is not available or not working.")
+  }
+}
+
+const getCardData = (data) => {
+  const modData = {}
+
+  for (const mod of data) {
+    if (modData[mod.name]) {
+      modData[mod.name].timesSold += 1
+    } else {
+      let type = mod.type
+      if (type.includes("Armor")) {
+        type = "Armor Mod"
+      }
+      if (type === "Legendary Weapon Mod") {
+        type = "Weapon Mod"
+      }
+      if (type === "Common Charged with Light Mod") {
+        type = "Charged with Light Mod"
+      }
+      if (type === "Common Warmind Cell Mod") {
+        type = "Warmind Cell Mod"
+      }
+
+      modData[mod.name] = {
+        name: mod.name,
+        type: type,
+        description: mod.description,
+        icon: mod.icon,
+        timesSold: 1,
+        // eslint-disable-next-line newline-per-chained-call
+        lastSold: new Date(mod.timestamp).toISOString().split("T")[0]
+      }
+    }
+  }
+
+  return Object.values(modData)
+}
+
+const getChargedWithLightModTimelineData = (data) => {
+  const modsTypesByName = {}
+  for (const mod of data) {
+    let type = mod.type
+    if (type === "Common Charged with Light Mod") {
+      type = "Charged with Light Mod"
+    }
+    if (type === "Charged with Light Mod") {
+      if (modsTypesByName[mod.name] === undefined) {
+        modsTypesByName[mod.name] = { timesSold: 1 }
+      }
+      modsTypesByName[mod.name].timesSold += 1
+    }
+  }
+
+  const result = []
+  for (const [key, value] of Object.entries(modsTypesByName)) {
+    result.push({ name: key, ...value })
+  }
+  return result
+}
+
+const getWarmindCellModTimelineData = (data) => {
+  const modsTypesByName = {}
+  for (const mod of data) {
+    let type = mod.type
+    if (type === "Common Warmind Cell Mod") {
+      type = "Warmind Cell Mod"
+    }
+    if (type === "Warmind Cell Mod") {
+      if (modsTypesByName[mod.name] === undefined) {
+        modsTypesByName[mod.name] = { timesSold: 1 }
+      }
+      modsTypesByName[mod.name].timesSold += 1
+    }
+  }
+
+  const result = []
+  for (const [key, value] of Object.entries(modsTypesByName)) {
+    result.push({ name: key, ...value })
+  }
+  return result
+}
+
+export { getCardData, getModData, getChargedWithLightModTimelineData, getWarmindCellModTimelineData }
