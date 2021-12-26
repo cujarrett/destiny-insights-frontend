8e6c8bf6ac6c2c1b27f4c8115a0d95c9da45a5fb
Matt jarrett <cujarrett@gmail.com>
2021-12-26 15:03:10 -0600

    fix: updates dependencies react-scripts                    ~4.0.3   →  ~5.0.0 @commitlint/cli                  ~15.0.0  →  ~16.0.0 @commitlint/config-conventional  ~15.0.0  →  ~16.0.0

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
-import reportWebVitals from "./reportWebVitals"
+import reportWebVitals from "./report-web-vitals.js"
 import "font-awesome/css/font-awesome.min.css"
 
 ReactDOM.render(
@@ -12,7 +12,4 @@ ReactDOM.render(
   document.getElementById("root")
 )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
